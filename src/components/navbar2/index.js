8c08de7146effac1bff94c236dafb3d92f03b7e7
Ben Hopkins <bjhops17@gmail.com>
2021-02-24 17:27:53 -0800

    added card display.

--- src/components/navbar2/index.js
+++ src/components/navbar2/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
 
 function Navbar2() {
+
+
   return (
     <div>
       <nav id="lowerNav">
