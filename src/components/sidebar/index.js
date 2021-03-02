12939966e195dece8bdf6bbd9e0ed223390b276f
Kevin Hsieh <khsieh95@gmail.com>
2021-03-02 14:23:42 -0800

    fixed nodemailer and finished app pathing

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,27 +1,26 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
-import API from "../../api/product-routes"
-
+import API from "../../api/product-routes";
 
 function Sidebar() {
-
-  const [subCat, setSubCat] = useState ({
-    subcat: []
-  })
-  
-
+  const [subCat, setSubCat] = useState({
+    subcat: [],
+  });
 
   useEffect(() => {
-    API.getAllSubCategories().then(res => setSubCat({ subcat: res.data}))
-  }, [])
-
+    API.getAllSubCategories().then((res) => setSubCat({ subcat: res.data }));
+  }, []);
 
   return (
     <div className="sidebar">
       <aside>
-
-       {subCat.subcat.map(item => <button data-id={item.id} >{item.name}</button>)} 
-        
+        <div className="row">
+          <div className="col m12">
+            {subCat.subcat.map((item) => (
+              <button data-id={item.id}>{item.name}</button>
+            ))}
+          </div>
+        </div>
       </aside>
     </div>
   );
